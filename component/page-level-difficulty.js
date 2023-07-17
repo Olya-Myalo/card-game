d52c0ddcf684f19e6b5ab2abb27c1c2ca76d3802
Olya-Myalo <olyamyalo@mail.ru>
2023-07-17 18:16:53 +0300

    доработана страница с игрой

--- component/page-level-difficulty.js
+++ component/page-level-difficulty.js
@@ -1,6 +1,10 @@
+import {cards} from "./array-сards.js";
 
 export function renderPageLevelDifficulty(difficulty) {
-  const appHtml = `
+    const shuffledCards = shuffle([...cards, ...cards]);
+    const app = document.querySelector('#app');
+
+    const appHtml = `
       <div class="page-maps">
         <div class="page_header">
           <div class="time">
@@ -15,118 +19,128 @@ export function renderPageLevelDifficulty(difficulty) {
           </div>
         </div>
         <div class="cards">
-                <div class="reverse_side">
-                <img src="image/ace_of_spades.svg" alt="Туз пики">
-                </div>
-                <div>
-                <img src="image/king_of_spades.svg" alt="Король пики">
-                </div>
-                <div>
-                <img src="image/lady_of_spades.svg" alt="Дама пики">
-                </div>
-                <div>
-                <img src="image/jack_of_spades.svg" alt="Валет пики">
-                </div>
-                <div>
-                <img src="image/ten_peaks.svg" alt="10 пики">
-                </div>
-                <div>
-                    <img src="image/nine_peaks.svg" alt="9 пики">
-                </div>
-                <div>
-                    <img src="image/eight_peaks.svg" alt="8 пики">
-                </div>
-                <div>
-                    <img src="image/seven_peaks.svg" alt="7 пики">
-                </div>
-                <div>
-                    <img src="image/six_peaks.svg" alt="6 пики">
-                </div>
+        ${renderCards(difficulty, shuffledCards)}
+        </div>
+      </div>
+    `;
+  
+    app.innerHTML = appHtml;
 
-                <div>
-                    <img src="image/ace_of_hearts.svg" alt="Туз черви">
-                </div>
-                <div>
-                    <img src="image/king_of_hearts.svg" alt="Король черви">
-                </div>
-                <div>
-                    <img src="image/lady_of_hearts.svg" alt="Дама черви">
-                </div>
-                <div>
-                    <img src="image/jack_of_hearts.svg" alt="Валет черви">
-                </div>
-                <div>
-                    <img src="image/ten_worms.svg" alt="10 черви">
-                </div>
-                <div>
-                    <img src="image/nine_hearts.svg" alt="9 черви">
-                </div>
-                <div>
-                    <img src="image/eight_hearts.svg" alt="8 черви">
-                </div>
-                <div>
-                    <img src="image/seven_hearts.svg" alt="7 черви">
-                </div>
-                <div>
-                    <img src="image/six_hearts.svg" alt="6 черви">
-                </div>
-                <div>
-                    <img src="image/ace_of_diamonds.svg" alt="Туз бубны">
-                </div>
-                <div>
-                    <img src="image/king_of_diamonds.svg" alt="Король бубны">
-                </div>
-                <div>
-                    <img src="image/lady_of_diamonds.svg" alt="Дама бубны">
-                </div>
-                <div>
-                    <img src="image/jack_of_diamonds.svg" alt="Валет бубны">
-                </div>
-                <div>
-                    <img src="image/ten_diamonds.svg" alt="10 бубны">
-                </div>
-                <div>
-                    <img src="image/nine_diamonds.svg" alt="9 бубны">
-                </div>
-                <div>
-                    <img src="image/eight_diamonds.svg" alt="8 бубны">
-                </div>
-                <div>
-                    <img src="image/seven_diamonds.svg" alt="7 бубны">
-                </div>
-                <div>
-                    <img src="image/six_diamonds.svg" alt="6 бубны">
-                </div>
-                <div>
-                    <img src="image/ace_of_cross.svg" alt="Туз крести">
-                </div>
-                <div>
-                    <img src="image/king_of_cross.svg" alt="Король крести">
-                </div>
-                <div>
-                    <img src="image/lady_of_cross.svg" alt="Дама крести">
-                </div>
-                <div>
-                    <img src="image/jack_of_cross.svg" alt="Валет крести">
-                </div>
-                <div>
-                    <img src="image/ten_crosses.svg" alt="10 крести">
-                </div>
-                <div>
-                    <img src="image/nine_crosses.svg" alt="9 крести">
-                </div>
-                <div>
-                    <img src="image/eight_crosses.svg" alt="8 крести">
-                </div>
-                <div class="seven_crosses">
-                    <img src="image/seven_crosses.svg" alt="7 крести">
-                </div>
-                <div>
-                    <img src="image/six_crosses.svg" class="reverse_side" alt="6 крести">
-                </div>
-                `
+    const cardElements = document.querySelectorAll('.card');
+    cardElements.forEach(card => {
+      card.addEventListener('click', flipCard);
+    });
 
-                app.innerHTML = appHtml;
+    let memoryTimeoutId; 
+    memoryTimeoutId = setTimeout(() => {
+        cardElements.forEach(card => {
+          card.classList.remove('flipped');
+        });
+      }, 5000);
 
+    cardElements.forEach(card => {
+      card.classList.add('flipped');
+    });
 
+    const startTime = new Date().getTime();
+  
+    const timerValue = document.querySelector('.time-figure');
+    const timerInterval = setInterval(() => {
+      const currentTime = new Date().getTime();
+      const elapsedTime = currentTime - startTime;
+      const minutes = Math.floor(elapsedTime / 60000);
+      const seconds = Math.floor((elapsedTime % 60000) / 1000);
+      const formattedTime = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+      timerValue.textContent = formattedTime;
+    }, 1000);
+  
+    const restartButton = document.querySelector('#restart-button');
+    restartButton.addEventListener('click', () => {
+      clearInterval(timerInterval);
+      clearTimeout(memoryTimeoutId);
+      cardElements.forEach(card => {
+        card.classList.remove('flipped');
+      });
+      renderPageLevelDifficulty(difficulty);
+    });
+
+function renderCards(difficulty, cards) {
+  const numCards = getNumCards(difficulty) * 2;
+  const selectedCards = cards.slice(0,  Math.floor(numCards / 2));
+  const duplicatedCards = [...selectedCards, ...selectedCards];
+  const shuffledCards = shuffle(duplicatedCards);
+  let cardsHtml = '';
+  for (let i = 0; i < shuffledCards.length; i++) {
+    const card = shuffledCards[i];
+    const cardHtml = `
+    <div class="card">
+      <div class="card__back">
+        <img src="${card.front}" alt="">
+      </div>
+      <div class="card__front">
+        <img src="${card.back}" alt="">
+      </div>
+    </div>
+  `;
+    cardsHtml += cardHtml;
+  }
+  return cardsHtml;
+}
+}
+
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+
+function getNumCards(difficulty) {
+  switch (difficulty) {
+    case 'easy':
+      return 6;
+    case 'medium':
+      return 12;
+    case 'hard':
+      return 18;
+    default:
+      return 6;
+  }
+}
+
+function flipCard(event) {
+    const currentCard = event.currentTarget
+
+    if (
+        currentCard.classList.contains('flipped') ||
+        document.querySelectorAll('.flipped').length === 2
+    ) {
+        return
+    }
+
+    currentCard.classList.toggle('flipped')
+
+    const flippedCards = document.querySelectorAll('.flipped')
+
+    if (flippedCards.length === 2) {
+        const flippedCard1 = flippedCards[0]
+        const flippedCard2 = flippedCards[1]
+
+        if (flippedCard1.dataset.cardName === flippedCard2.dataset.cardName) {
+            alert('Вы победили!');
+        } else {
+            setTimeout(() => {
+                flippedCards.forEach((card) => {
+                    card.classList.add('flipped');
+                });
+                alert('Вы проиграли!');
+            }, 1000);
+        }
+    }
 }
